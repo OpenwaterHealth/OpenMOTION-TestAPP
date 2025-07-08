5b115737a2b101a2df361446164eb4da82b6cdeb
George Vigelette <george@openwater.cc>
2025-07-08 10:55:37 -0400

    update fpga models

--- models/FpgaModel.js
+++ models/FpgaModel.js
@@ -15,6 +15,11 @@ var fpgaAddressModel = [
             { name: "PWM MON CL", desc: "PWM Monitor Current Limit", start_address: 0x0A, data_size: "16B", direction: "RW", unit: "mv", scale: 0.500 },
             { name: "CW MON CL", desc: "CW Monitor Current Limit", start_address: 0x0C, data_size: "16B", direction: "RW", unit: "mv", scale: 0.500 },
             { name: "TEMP Sensor", desc: "Temperature Sensor", start_address: 0x0E, data_size: "16B", direction: "RW" },
+            { name: "TRIGGER COUNT", desc: "Trigger Count", start_address: 0x10, data_size: "32B", direction: "RD" },
+            { name: "REVISION", desc: "Revision Version", start_address: 0x14, data_size: "8B", direction: "RD" },
+            { name: "MINOR", desc: "Minor Version", start_address: 0x15, data_size: "8B", direction: "RD" },
+            { name: "MAJOR", desc: "Major Version", start_address: 0x16, data_size: "8B", direction: "RD" },
+            { name: "ID", desc: "FPGA ID", start_address: 0x17, data_size: "8B", direction: "RD" },
             { name: "STATIC CTL", desc: "Static Control", start_address: 0x20, data_size: "16B", direction: "RW" },
             { name: "DYNAMIC CTL", desc: "Dynamic Control", start_address: 0x22, data_size: "16B", direction: "RW" },
             { name: "STATUS", desc: "Status Register", start_address: 0x24, data_size: "8B", direction: "RW" }
@@ -28,8 +33,8 @@ var fpgaAddressModel = [
         isMsbFirst: false,
         functions: [
             { name: "DDS CTRL", desc: "DDS Control", start_address: 0x00, data_size: "16B", direction: "RW" },
-            { name: "DDS CURRENT", desc: "DDS Current", start_address: 0x02, data_size: "16B", direction: "RW", unit: "mA", scale: 0.064 },
-            { name: "CW CURRENT", desc: "CW Current", start_address: 0x04, data_size: "16B", direction: "RW", unit: "mA", scale: 0.064 },
+            { name: "DDS GAIN", desc: "DDS Gain", start_address: 0x02, data_size: "16B", direction: "RW", unit: "mA", scale: 0.064 },
+            { name: "CW GAIN", desc: "CW Gain", start_address: 0x04, data_size: "16B", direction: "RW", unit: "mA", scale: 0.064 },
             { name: "DDS CL", desc: "DDS Current Limit", start_address: 0x06, data_size: "16B", direction: "RW", unit: "mA", scale: 0.019 },
             { name: "CW CL", desc: "CW Current Limit", start_address: 0x08, data_size: "16B", direction: "RW", unit: "mA", scale: 0.019 },
             { name: "ADC DDS CL", desc: "ADC DDS Current Limit", start_address: 0x0A, data_size: "16B", direction: "RW", unit: "mv", scale: 0.500 },
@@ -37,6 +42,10 @@ var fpgaAddressModel = [
             { name: "ADC CD", desc: "ADC Current Data", start_address: 0x0E, data_size: "16B", direction: "RD", unit: "mv", scale: 0.500 },
             { name: "ADC VD", desc: "ADC Voltage Data", start_address: 0x10, data_size: "16B", direction: "RD", unit: "mv", scale: 0.500 },
             { name: "STATUS", desc: "Status", start_address: 0x12, data_size: "8B", direction: "RD" },
+            { name: "REVISION", desc: "Revision Version", start_address: 0x13, data_size: "8B", direction: "RD" },
+            { name: "MINOR", desc: "Minor Version", start_address: 0x14, data_size: "8B", direction: "RD" },
+            { name: "MAJOR", desc: "Major Version", start_address: 0x15, data_size: "8B", direction: "RD" },
+            { name: "ID", desc: "FPGA ID", start_address: 0x16, data_size: "8B", direction: "RD" },
             { name: "STATIC CTRL", desc: "Static Control", start_address: 0x20, data_size: "16B", direction: "RW" },
             { name: "DYNAMIC CTRL", desc: "Dynamic Control", start_address: 0x22, data_size: "16B", direction: "WR" }
         ]
@@ -58,9 +67,14 @@ var fpgaAddressModel = [
             { name: "PWM MONITOR CL", desc: "PWM Monitor Current Limit", start_address: 0x16, data_size: "16B", direction: "RW", unit: "mv", scale: 0.500 },
             { name: "CW MONITOR CL", desc: "CW Monitor Current Limit", start_address: 0x18, data_size: "16B", direction: "RW", unit: "mv", scale: 0.500 },
             { name: "TEMP Sensor", desc: "Temperature Sensor", start_address: 0x1A, data_size: "16B", direction: "RW" },
+            { name: "ADC DATA", desc: "ADC Data", start_address: 0x1C, data_size: "16B", direction: "RD" },
             { name: "STATIC CTRL", desc: "Static control bits", start_address: 0x20, data_size: "16B", direction: "RW" },
             { name: "DYNAMIC CTRL", desc: "Dynamic control bits", start_address: 0x22, data_size: "16B", direction: "WR" },
-            { name: "STATUS", desc: "Status Register", start_address: 0x24, data_size: "8B", direction: "RW" }
+            { name: "STATUS", desc: "Status Register", start_address: 0x24, data_size: "8B", direction: "RW" },
+            { name: "REVISION", desc: "Revision Version", start_address: 0x25, data_size: "8B", direction: "RD" },
+            { name: "MINOR", desc: "Minor Version", start_address: 0x26, data_size: "8B", direction: "RD" },
+            { name: "MAJOR", desc: "Major Version", start_address: 0x27, data_size: "8B", direction: "RD" },
+            { name: "ID", desc: "FPGA ID", start_address: 0x28, data_size: "8B", direction: "RD" }
         ]
     },
     {
@@ -80,9 +94,14 @@ var fpgaAddressModel = [
             { name: "PWM MONITOR CL", desc: "PWM Monitor Current Limit", start_address: 0x16, data_size: "16B", direction: "RW", unit: "mv", scale: 0.500 },
             { name: "CW MONITOR CL", desc: "CW Monitor Current Limit", start_address: 0x18, data_size: "16B", direction: "RW", unit: "mv", scale: 0.500 },
             { name: "TEMP Sensor", desc: "Temperature Sensor", start_address: 0x1A, data_size: "16B", direction: "RW" },
+            { name: "ADC DATA", desc: "ADC Data", start_address: 0x1C, data_size: "16B", direction: "RD" },
             { name: "STATIC CTRL", desc: "Static control bits", start_address: 0x20, data_size: "16B", direction: "RW" },
             { name: "DYNAMIC CTRL", desc: "Dynamic control bits", start_address: 0x22, data_size: "16B", direction: "WR" },
-            { name: "STATUS", desc: "Status Register", start_address: 0x24, data_size: "8B", direction: "RW" }
+            { name: "STATUS", desc: "Status Register", start_address: 0x24, data_size: "8B", direction: "RW" },
+            { name: "REVISION", desc: "Revision Version", start_address: 0x25, data_size: "8B", direction: "RD" },
+            { name: "MINOR", desc: "Minor Version", start_address: 0x26, data_size: "8B", direction: "RD" },
+            { name: "MAJOR", desc: "Major Version", start_address: 0x27, data_size: "8B", direction: "RD" },
+            { name: "ID", desc: "FPGA ID", start_address: 0x28, data_size: "8B", direction: "RD" }
         ]
     }
 ];
